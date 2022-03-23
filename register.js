3321a3b09d7ba3811647c3959e468f9bd8f62264
saurabhyeggewar1998 <yeggewar@gmail.com>
2022-03-23 21:11:41 +0530

    UserRegistration With Regex

---
+++ register.js
@@ -0,0 +1,44 @@
+//UC1 first name
+// let name="Saurabh"
+// let nregx=/^([A-Z]){1}([a-z]{4,})$/;
+// if(nregx.test(name)){
+//     console.log("successfull");
+// }
+// else{
+//     console.log("unsuccessfull");
+// }
+
+//uc2 password
+// let pass="Saurabhyeggewar2#"
+// let ppregx = /^(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[A-Z]).{8,}$/;
+// if(ppregx.test(pass)){
+//     console.log("successfull");
+// }
+// else{
+//     console.
+//     log("unsuccessfull");
+// }
+
+
+//uc3 email
+
+// let email="Saurabhyeggewar@gmail.com";
+// let eregx=/^([a-zA-Z0-9_]+)(.)([a-zA-Z0-9_]+)(@)([a-zA-Z0-9_]+)(.)([a-zA-Z0-9_]+)(.)([a-zA-Z0-9_]+)$/;
+// if(eregx.test(email)){
+//     console.log("successfull");
+// }
+// else{
+//     console.log("unsuccessfull");
+// }
+
+//uc4 phonenumber
+
+let phone="91 9922992299";
+let pregx=/^([0-9]+)[ ]([0-9]{10})$/;
+if(pregx.test(phone)){
+    console.log("successfull");
+}
+else{
+    console.
+    log("unsuccessfull");
+}
